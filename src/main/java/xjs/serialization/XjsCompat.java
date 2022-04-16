6571de0fac2ea187744d2011fbc4fd56b9639672
PersonTheCat <professionallyperson@gmail.com>
2022-04-16 12:15:23 -0500

    Better loading strategy

--- src/main/java/xjs/serialization/XjsCompat.java
+++ src/main/java/xjs/serialization/XjsCompat.java
@@ -7,15 +7,13 @@ import xjs.serialization.writer.HjsonWriter;
 /**
  * Core utilities used by xjs-compat. For internal use only.
  */
+@ApiStatus.Internal
 public final class XjsCompat {
 
     private XjsCompat() {}
 
-    /**
-     * Loads the provided serializers into the {@link JsonSerializationContext}.
-     */
-    @ApiStatus.Internal
-    public static void init() {
+    // Loads the provided serializers into the JsonSerializationContext.
+    static {
         JsonSerializationContext.addParser("hjson", file -> new HjsonParser(file).parse());
         JsonSerializationContext.addWriter("hjson", (w, v, o) -> new HjsonWriter(w, o).write(v));
     }
