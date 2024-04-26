4c15be709c04db8ff99e4c6bb5ee9b452d0a4120
PersonTheCat <professionallyperson@gmail.com>
2024-04-25 23:19:07 -0500

    Update performance tests

--- src/test/java/xjs/compat/performance/PerformanceTest.java
+++ src/test/java/xjs/compat/performance/PerformanceTest.java
@@ -7,12 +7,18 @@ import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Threads;
 import xjs.compat.serialization.parser.HjsonParser;
+import xjs.compat.serialization.parser.UbjsonParser;
+import xjs.compat.serialization.util.UBTyping;
 import xjs.compat.serialization.writer.HjsonWriter;
+import xjs.compat.serialization.writer.UbjsonWriter;
 import xjs.data.Json;
 import xjs.data.JsonCopy;
 import xjs.data.JsonValue;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringWriter;
 import java.util.concurrent.TimeUnit;
 
@@ -37,21 +43,45 @@ public class PerformanceTest {
           '''
         """;
 
-    private static final JsonValue DJS_WRITING_SAMPLE =
+    private static final JsonValue WRITING_SAMPLE =
         Json.parse(HJSON_SAMPLE).copy(JsonCopy.UNFORMATTED | JsonCopy.COMMENTS);
+
+    private static final byte[] SIMPLE_UBJSON_SAMPLE;
+
+    static {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            new UbjsonWriter(out, UBTyping.BALANCED).write(WRITING_SAMPLE);
+        } catch (IOException ignored) {
+            throw new AssertionError("unreachable");
+        }
+        SIMPLE_UBJSON_SAMPLE = out.toByteArray();
+    }
 
     public static void main(final String... args) throws Exception {
         LocalBenchmarkRunner.runIfEnabled();
     }
 
-    @Enabled(true)
+    @Enabled(false)
     @Benchmark
     @Fork(2)
     @Threads(4)
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    public JsonValue djsParsingSample() {
+    public JsonValue hjsonParsingSample() {
         return new HjsonParser(SIMPLE_HJSON_SAMPLE).parse();
+    }
+
+    @Enabled(false)
+    @Benchmark
+    @Fork(2)
+    @Threads(4)
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public String hjsonWritingSample() throws IOException {
+        final StringWriter sw = new StringWriter();
+        new HjsonWriter(sw, true).write(WRITING_SAMPLE);
+        return sw.toString();
     }
 
     @Enabled(true)
@@ -60,9 +90,20 @@ public class PerformanceTest {
     @Threads(4)
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    public String djsWritingSample() throws IOException {
-        final StringWriter sw = new StringWriter();
-        new HjsonWriter(sw, true).write(DJS_WRITING_SAMPLE);
-        return sw.toString();
+    public JsonValue ubjsonParsingSample() throws IOException {
+        final InputStream is = new ByteArrayInputStream(SIMPLE_UBJSON_SAMPLE);
+        return new UbjsonParser(is).parse();
+    }
+
+    @Enabled(true)
+    @Benchmark
+    @Fork(2)
+    @Threads(4)
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public byte[] ubjsonWritingSample() throws IOException {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        new UbjsonWriter(out, UBTyping.BALANCED).write(WRITING_SAMPLE);
+        return out.toByteArray();
     }
 }
